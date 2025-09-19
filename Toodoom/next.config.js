46ddee52f264abdd0509bac17536eab53de3496a
ivucicev <ivucicev@gmail.com>
2025-09-19 14:25:17 +0200

    .

--- Toodoom/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-    async rewrites() {
-        return [
-            { source: "/api/:path*", destination: `${process.env.BACKEND_URL}/api/:path*` },
-        ];
-    },
-};
