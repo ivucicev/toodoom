cfd6016efed71372346acd2a8e14a7c7856b95ac
ivucicev <ivucicev@gmail.com>
2025-10-16 14:07:08 +0000

    fix comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { JsonPipe } from '@angular/common';
 import { distinctUntilChanged, fromEvent, map, merge, shareReplay } from 'rxjs';
 import { SwUpdate } from '@angular/service-worker';
 
+// header tabs
 type AppMode = 'tasks' | 'notes' | 'notepad';
 
 @Component({
