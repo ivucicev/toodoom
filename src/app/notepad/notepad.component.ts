2d41f9e8dd5ec435531cd9e4e8968e16378f6289
CI <ci@example.com>
2025-12-25 06:30:35 +0000

    fix: minor comment tweak in Notepad component

--- src/app/notepad/notepad.component.ts
+++ src/app/notepad/notepad.component.ts
@@ -46,7 +46,7 @@ export class NotepadComponent implements OnDestroy {
 		}
 	}
 
-	async selectNotepad(id: string) { // called when a notepad is selected
+	async selectNotepad(id: string) { // called when a notepad is selected (id may be empty)
 		if (this.saveTimer) {
 			clearTimeout(this.saveTimer);
 			this.saveTimer = null;
