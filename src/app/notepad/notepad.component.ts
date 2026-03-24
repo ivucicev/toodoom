ab5921b3124a2e2db07a9da4f9fcfe5a95fe1544
CI <ci@example.com>
2026-03-24 06:31:13 +0000

    fix: add clarifying comment to Notepad component

--- src/app/notepad/notepad.component.ts
+++ src/app/notepad/notepad.component.ts
@@ -9,6 +9,7 @@ import { ToastService } from '../core/toast.service';
 	templateUrl: './notepad.component.html',
 	styleUrl: './notepad.component.css'
 })
+// NOTE: Lightweight component for notes
 export class NotepadComponent implements OnDestroy {
 
 	public notepads: INotepad[] = [];
